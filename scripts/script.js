91365de1382459b4adaa138c0448643125e2dc1b
Ann Karpinski <annkarpinski@gmail.com>
2020-04-26 17:12:26 -0600

    added scheduler layout

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,26 @@ $(document).ready(function () {
   //Display current date
   var today = moment().format("dddd, MMMM Do");
   $("#currentDay").text(today);
+
+  //create array of hours
+  var hour = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+  for (var i = 0; i < hour.length; i++) {
+    //Create new html elements
+    var newDiv = $("<div>");
+    var newP = $("<p>");
+    var newInput = $("<input>");
+    var newBtn = $("<button>");
+
+    //Add class styles to new html elements
+    newDiv.addClass("row");
+    newP.addClass("col-sm-1 time-block hour");
+    newP.text(hour[i]);
+    newInput.addClass("col-sm-10 description");
+    newBtn.addClass("col-sm-1 saveBtn");
+
+    //Appending new elements to the DOM
+    newDiv.append(newP, newInput, newBtn);
+    $(".container").append(newDiv);
+  }
 });
