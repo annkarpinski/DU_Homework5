e56a0fa5468670c5582cdd953c3406198b2acbb5
Ann Karpinski <annkarpinski@gmail.com>
2020-04-25 18:38:45 -0600

    initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,2 @@
 //Code for Workday Scheduler
-$(document).ready(function () {
-  //Display current date
-  var today = moment().format("dddd, MMMM Do");
-  $("#currentDay").text(today);
-});
+$(document).ready(function () {});
